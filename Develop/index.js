d4067e20a6bf994d8cba608aa0bf663856acd75f
Brandi Murray <pritchettb14@gmail.com>
2021-04-15 16:16:47 -0400

    README template, questions and functionality

--- Develop/index.js
+++ Develop/index.js
@@ -2,10 +2,17 @@
 const inquirer = require("inquirer");
 //external module ^^
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 //^^ core module
+
 
 // TODO: Create an array of questions for user input
 const questions = [
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "title",
+  },
   {
     type: "input",
     message: "What is your Github Username?",
@@ -16,41 +23,62 @@ const questions = [
     message: "What is your e-mail address?",
     name: "email",
   },
-  {
-    type: "input",
-    message: "What is your project's name?",
-    name: "title",
-  },
+ 
   {
     type: "input",
     message: "Please write a short description of your project..",
     name: "description",
   },
   {
+    type: "input",
+    message: "Please provide details on how to use this application.",
+    name: "usage",
+  },
+  {
     type: "list",
-    message: "What kind of license should your project have?",
+    message: "What kind of license should your project have? ",
     name: "license",
-    choices: ["MIT", "", "Text message"],
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
   },
   {
     type: "input",
-    message: "What is your Github URL?",
-    name: "githubURL",
-  },
-  {
-    type: "input",
-    message: "Please provide us with the URL to your LinkedIn.",
-    name: "linkedin",
+    message: "What command should be run to install dependencies?",
+    name: "install",
   },
  
+  {
+    type: "input",
+    message: "List the technologies used in this project..",
+    name: "tech",
+  },
+  {
+    type: "input",
+    message: "Advise users on how they can contribute to this project",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Would you like to add any images? (to input image, open an issue in github, drop screenshot into open issue and paste the link)",
+    name: "img",
+  },
+  {
+    type: "input",
+    message: "Describe the testing process for this app.",
+    name: "test",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, content) {
+  fs.writeFileSync(fileName,content)
+}
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then();
+  inquirer.prompt(questions).then(function(data){
+   const content = generateMarkdown(data)
+    writeToFile("README.md",content)
+  });
 }
 
 // Function call to initialize app
